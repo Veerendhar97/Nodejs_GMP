cfee5567732fdece8a68a30be2b156b8ac398b51
veerendhar_arepally <Veerendhar_Arepally@epam.com>
2021-05-11 22:38:29 +0530

    Task_1: includes task 1.3

--- Task_1/Task_1.2.js
+++ Task_1/Task_1.2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const csv = require('csvtojson');
 
 const csvFilePath = 'files/info.csv';
-const logFilePath = 'Homework_1/log_1.txt';
+const logFilePath = 'files/log_1.txt';
 
 const readFromCsv = fs.createReadStream(csvFilePath);
 const writeToLog = fs.createWriteStream(logFilePath);
